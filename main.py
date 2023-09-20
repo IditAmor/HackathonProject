26553c17979c6e6d2ab28019b7af67a7bceb3f6f
IditAmor <idit.amor111@gmail.com>
2023-09-20 12:41:39 +0300

    dict format

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+name = ''
+phone = ''
+more_details = ''
+
+my_dict = {
+    'animals': [[name, phone, more_details], [name, phone, more_details], [name, phone, more_details],
+                [name, phone, more_details], [name, phone, more_details], [name, phone, more_details]],
+    'elderly': [[name, phone, more_details], [name, phone, more_details], [name, phone, more_details],
+                [name, phone, more_details], [name, phone, more_details], [name, phone, more_details]],
+    'sick people': [[name, phone, more_details], [name, phone, more_details], [name, phone, more_details],
+                    [name, phone, more_details], [name, phone, more_details], [name, phone, more_details]],
+    'people with disabilities': [[name, phone, more_details], [name, phone, more_details], [name, phone, more_details],
+                                 [name, phone, more_details], [name, phone, more_details], [name, phone, more_details]]
+}
 
