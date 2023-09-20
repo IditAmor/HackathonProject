5ddc1a49919f8fab029239c90c0a62d0a922c58a
IditAmor <idit.amor111@gmail.com>
2023-09-20 12:43:28 +0300

    dict format fix

--- main.py
+++ main.py
@@ -1,15 +1,1 @@
-name = ''
-phone = ''
-more_details = ''
-
-my_dict = {
-    'animals': [[name, phone, more_details], [name, phone, more_details], [name, phone, more_details],
-                [name, phone, more_details], [name, phone, more_details], [name, phone, more_details]],
-    'elderly': [[name, phone, more_details], [name, phone, more_details], [name, phone, more_details],
-                [name, phone, more_details], [name, phone, more_details], [name, phone, more_details]],
-    'sick people': [[name, phone, more_details], [name, phone, more_details], [name, phone, more_details],
-                    [name, phone, more_details], [name, phone, more_details], [name, phone, more_details]],
-    'people with disabilities': [[name, phone, more_details], [name, phone, more_details], [name, phone, more_details],
-                                 [name, phone, more_details], [name, phone, more_details], [name, phone, more_details]]
-}
-
+# main
